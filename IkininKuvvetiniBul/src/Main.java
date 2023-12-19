001ce75ab79811ed3ebeb41021198561ca08a36c
boranesin <59020208+boranesin@users.noreply.github.com>
2023-12-20 02:38:09 +0300

    Update Main.java

--- IkininKuvvetiniBul/src/Main.java
+++ IkininKuvvetiniBul/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Scanner girdi = new Scanner(System.in);
         System.out.println("Lütfen Bir Sayı giriniz: ");
         n = girdi.nextInt();
-
+            // burada alınan n degeri kadar donguyu donduruyor ve her dongu de 2 nin bir ustu sayıyı elde ediyor
         for (int i = 1; i <=n; i*=2 ) {
             System.out.println(i);
         }
